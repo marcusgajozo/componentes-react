ae06bd27790e742b19069e1b316c8a9fabc93033
Marcus Gajozo <marcusgajozo@gmail.com>
2026-08-04 19:31:06 -0400

    refactor(select): melhora tipagem do componente FormSelect

--- src/features/select/ui/components/form-select/form-select.tsx
+++ src/features/select/ui/components/form-select/form-select.tsx
@@ -1,14 +1,20 @@
-import { Controller, useFormContext } from "react-hook-form";
+import { type Control, Controller, type FieldPath, type FieldValues } from "react-hook-form";
 
 import { Select, type SelectProps } from "../select/select";
 
-export interface FormSelectProps extends Omit<SelectProps, "value" | "onChange" | "name"> {
-  name: string;
-}
+export type FormSelectProps<TFieldValues extends FieldValues> = Omit<
+  SelectProps,
+  "value" | "onChange" | "name"
+> & {
+  name: FieldPath<TFieldValues>;
+  control: Control<TFieldValues>;
+};
 
-export function FormSelect({ name, ...props }: FormSelectProps) {
-  const { control } = useFormContext();
-
+export function FormSelect<TFieldValues extends FieldValues>({
+  name,
+  control,
+  ...props
+}: FormSelectProps<TFieldValues>) {
   return (
     <Controller
       name={name}
